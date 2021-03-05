ffa0e3fac26c6886383e408f215ba9ad5ea0aba6
BRTEK007 <bartoszkonderla@gmail.com>
2021-03-05 15:05:39 +0100

    pathfinding

--- pathfinding/script.js
+++ pathfinding/script.js
@@ -1,110 +1,182 @@
 var canvas, ctx;
 var solver;
-var painting;
 
-var settings = {
+
+var painting, brush = 0;
+const BRUSH = {
+  WALL: 0,
+  EMPTY: 1,
+  START: 2,
+  END: 3
+}
+
+const COLORS = {
+  GRID_LINE: "#7d7d7d",
+  WALL: "white",
+  EMPTY: "black",
+  START_NODE: "green",
+  END_NODE: "red"
+}
+
+const WALL = true, EMPTY = false;
+
+const SETTINGS = {
   size: 30,
   algorythm: 'A*',
   diagonal: true,
   diagonal: 51,
 }
 
-var grid = {
+const GRID = {
+  cells: [],//false = empty, true = wall
   cell_size: 30,
   width: 20,
   height: 25,
   offset_x: 10,
-  offset_y: 10
+  offset_y: 10,
+  startNode: { x: 1, y: 1 },
+  endNode: { x: 10, y: 10 }
 }
 
-function drawGrid(){
-  ctx.clearRect(0,0,canvas.width, canvas.height);
-  ctx.strokeStyle = "white";
-  for(var x = 1; x < grid.width; x++){
+function drawGrid() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = COLORS.GRID_LINE;
+  for (var x = 1; x < GRID.width; x++) {
     ctx.beginPath();
-    ctx.moveTo(grid.offset_x + (1+grid.cell_size) * x, 0);
-    ctx.lineTo(grid.offset_x + (1+grid.cell_size) * x, canvas.height);
+    ctx.moveTo(GRID.offset_x + (1 + GRID.cell_size) * x, 0);
+    ctx.lineTo(GRID.offset_x + (1 + GRID.cell_size) * x, canvas.height);
     ctx.stroke();
   }
-  for(var y = 1; y < grid.height; y++){
+  for (var y = 1; y < GRID.height; y++) {
     ctx.beginPath();
-    ctx.moveTo(0, grid.offset_y + (1+grid.cell_size) * y);
-    ctx.lineTo(canvas.width , grid.offset_y + (1+grid.cell_size) * y);
+    ctx.moveTo(0, GRID.offset_y + (1 + GRID.cell_size) * y);
+    ctx.lineTo(canvas.width, GRID.offset_y + (1 + GRID.cell_size) * y);
     ctx.stroke();
   }
+  ctx.fillStyle = COLORS.START_NODE;
+  drawCell(GRID.startNode.x, GRID.startNode.y);
+  ctx.fillStyle = COLORS.END_NODE;
+  drawCell(GRID.endNode.x, GRID.endNode.y);
 }
 
 function updateGridDimensions() {
-  grid.cell_size = settings.size;
-  grid.width = Math.floor((canvas.width - 1) / (grid.cell_size + 1));
-  grid.height = Math.floor((canvas.height - 1) / (grid.cell_size + 1));
-  grid.offset_x = Math.floor((canvas.width - (grid.cell_size + 1) * grid.width) / 2);
-  grid.offset_y = Math.floor((canvas.height - (grid.cell_size + 1) * grid.height) / 2);
+  GRID.cell_size = SETTINGS.size;
+  GRID.width = Math.floor((canvas.width - 1) / (GRID.cell_size + 1));
+  GRID.height = Math.floor((canvas.height - 1) / (GRID.cell_size + 1));
+  GRID.offset_x = Math.floor((canvas.width - (GRID.cell_size + 1) * GRID.width) / 2);
+  GRID.offset_y = Math.floor((canvas.height - (GRID.cell_size + 1) * GRID.height) / 2);
+  GRID.cells = new Array(GRID.width * GRID.height).fill(EMPTY);
 }
 
 function changeSettings(param, val) {
   switch (param) {
     case 'SIZE':
-      settings.size = parseInt(val);
+      SETTINGS.size = parseInt(val);
       updateGridDimensions();
       drawGrid();
       break;
     case 'ALGORYTHM':
-      settings.algorythm = val;
+      SETTINGS.algorythm = val;
       break;
     case 'SPACING':
-      settings.diagonal = val;
+      SETTINGS.diagonal = val;
       break;
     case 'SPEED':
-      settings.speed = parseInt(val);
+      SETTINGS.speed = parseInt(val);
       break;
   }
+}
+
+function mouseCordsToCellCords(mx, my) {
+  let cx = Math.floor((mx - GRID.offset_x) / (GRID.cell_size + 1));
+  cx = Math.min(cx, GRID.width - 1);
+  cx = Math.max(cx, 0);
+  let cy = Math.floor((my - GRID.offset_y) / (GRID.cell_size + 1));
+  cy = Math.min(cy, GRID.height - 1);
+  cy = Math.max(cy, 0);
+  return { x: cx, y: cy }
+}
+
+function drawCell(cx, cy) {
+  let x = GRID.offset_x + cx * (GRID.cell_size + 1);
+  let y = GRID.offset_y + cy * (GRID.cell_size + 1);
+  let w = GRID.cell_size;
+  let h = GRID.cell_size;
+  if (cx == 0) {
+    w += GRID.offset_x;
+    x -= GRID.offset_x;
+  } else if (cx == GRID.width - 1) {
+    w += GRID.offset_x + 2;
+  }
+  if (cy == 0) {
+    h += GRID.offset_y;
+    y -= GRID.offset_y;
+  } else if (cy == GRID.height - 1) {
+    h += GRID.offset_y + 2;
+  }
+  //ctx.fillStyle = "white";
+  ctx.fillRect(x + 1, y + 1, w - 1, h - 1);
 }
 
 function pageLoaded() {
   canvas = document.getElementById('canvas1');
 
-  canvas.addEventListener('mousemove', e => {
-    if(!painting) return;
-    let cx = Math.floor((e.offsetX-grid.offset_x) / (grid.cell_size + 1));
-    cx = Math.min(cx, grid.width-1);
-    cx = Math.max(cx, 0);
-    let cy = Math.floor((e.offsetY-grid.offset_y) / (grid.cell_size + 1));
-    cy = Math.min(cy, grid.height-1);
-    cy = Math.max(cy, 0);
-
-    let x = grid.offset_x + cx*(grid.cell_size+1);
-    let y = grid.offset_y + cy*(grid.cell_size+1);
-    let w = grid.cell_size;
-    let h = grid.cell_size;
-    if(cx == 0){
-       w += grid.offset_x;
-       x -= grid.offset_x;
-    }else if(cx == grid.width-1){
-      w += grid.offset_x;
-    }
-    if(cy == 0){
-      h += grid.offset_y;
-      y -= grid.offset_y;
-   }else if(cy == grid.height-1){
-     h += grid.offset_y;
-   }
-    ctx.fillStyle = "white";
-    ctx.fillRect(x, y,w, h);
-    //draw cell rect function (cx, cy)
-  });
-
-  canvas.addEventListener('mouseleave', e => {
-    painting = false;
-  });
-
   canvas.addEventListener('mousedown', e => {
     painting = true;
+
+    let cords = mouseCordsToCellCords(e.offsetX, e.offsetY);
+
+    if (cords.x == GRID.startNode.x && cords.y == GRID.startNode.y) {
+    brush = BRUSH.START;
+    } else if (cords.x == GRID.endNode.x && cords.y == GRID.endNode.y) {
+     brush = BRUSH.END;
+    } else {
+      if (e.button == 0) {
+        ctx.fillStyle = COLORS.WALL;
+        brush = BRUSH.WALL;
+      }
+      else if (e.button == 2) {
+        ctx.fillStyle = COLORS.EMPTY;
+        brush = BRUSH.EMPTY;
+      }
+      drawCell(cords.x, cords.y);
+    }
+  });
+
+  canvas.addEventListener('mousemove', e => {
+    if (!painting) return;
+    let cords = mouseCordsToCellCords(e.offsetX, e.offsetY);
+
+    if(brush == BRUSH.START){
+      if (cords.x == GRID.endNode.x && cords.y == GRID.endNode.y) return;
+      ctx.fillStyle = COLORS.EMPTY;//fill colors.wall if there was one
+      drawCell(GRID.startNode.x, GRID.startNode.y);
+      ctx.fillStyle = COLORS.START_NODE;
+      drawCell(cords.x, cords.y);
+      GRID.startNode.x = cords.x;
+      GRID.startNode.y = cords.y;
+    }else if(brush == BRUSH.END){
+      if (cords.x == GRID.startNode.x && cords.y == GRID.startNode.y) return;
+      ctx.fillStyle = COLORS.EMPTY;//fill colors.wall if there was one
+      drawCell(GRID.endNode.x, GRID.endNode.y);
+      ctx.fillStyle = COLORS.END_NODE;
+      drawCell(cords.x, cords.y);
+      GRID.endNode.x = cords.x;
+      GRID.endNode.y = cords.y;
+    }
+
+    if (cords.x == GRID.startNode.x && cords.y == GRID.startNode.y) return;
+    else if (cords.x == GRID.endNode.x && cords.y == GRID.endNode.y) return;
+    drawCell(cords.x, cords.y);
   });
 
   canvas.addEventListener('mouseup', e => {
     painting = false;
   });
+  canvas.addEventListener('mouseleave', e => {
+    painting = false;
+  });
+
 
   canvas.height = canvas.getBoundingClientRect().height;
   canvas.width = canvas.getBoundingClientRect().width;
@@ -112,8 +184,4 @@ function pageLoaded() {
   updateGridDimensions();
   drawGrid();
   painting = false;
-}
-
-function mouseDown(){
-  console.log(4);
 }
