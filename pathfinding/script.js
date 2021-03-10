72ff6452ab5daa20472ce2b0e2357d21fe9ba5fa
BRTEK007 <bartoszkonderla@gmail.com>
2021-03-10 21:16:16 +0100

    bi

--- pathfinding/script.js
+++ pathfinding/script.js
@@ -6,7 +6,7 @@ var solver;
 const INPUT = {
   painting: false,
   brush: 0,
-  oldMouseCords: {x : null, y : null}
+  oldMouseCords: { x: null, y: null }
 }
 
 const BRUSH = {
@@ -110,13 +110,13 @@ function lineCords(x0, y0, x1, y1) {
     var step = (y1 - y0) / (x1 - x0);
     for (let x = x0; x <= x1; x++) {
       let y = y0 + Math.round((x - x0) * step);
-      arr.push(x,y);
+      arr.push(x, y);
     }
-  }else{
+  } else {
     var step = (x1 - x0) / (y1 - y0);
     for (let y = y0; y <= y1; y++) {
       let x = x0 + Math.round((y - y0) * step);
-      arr.push(x,y);
+      arr.push(x, y);
     }
   }
 
@@ -177,7 +177,7 @@ function pageLoaded() {
     INPUT.painting = true;
 
     let cords = mouseCordsToCellCords(e.offsetX, e.offsetY);
-    INPUT.oldMouseCords = {x: cords.x, y: cords.y};
+    INPUT.oldMouseCords = { x: cords.x, y: cords.y };
 
     if (cords.x == GRID.startCell.x && cords.y == GRID.startCell.y) {
       INPUT.brush = BRUSH.START;
@@ -205,7 +205,7 @@ function pageLoaded() {
     if (INPUT.brush == BRUSH.START) {
       if (cords.x == GRID.endCell.x && cords.y == GRID.endCell.y) return;
       let id = cords.x + cords.y * GRID.width;
-      if(GRID.cells[id] == WALL) return;
+      if (GRID.cells[id] == WALL) return;
       let oldId = GRID.startCell.x + GRID.startCell.y * GRID.width;
       ctx.fillStyle = GRID.cells[oldId] == WALL ? COLORS.WALL : COLORS.EMPTY;
       drawCell(GRID.startCell.x, GRID.startCell.y);
@@ -217,7 +217,7 @@ function pageLoaded() {
     } else if (INPUT.brush == BRUSH.END) {
       if (cords.x == GRID.startCell.x && cords.y == GRID.startCell.y) return;
       let id = cords.x + cords.y * GRID.width;
-      if(GRID.cells[id] == WALL) return;
+      if (GRID.cells[id] == WALL) return;
       let oldId = GRID.endCell.x + GRID.endCell.y * GRID.width;
       ctx.fillStyle = GRID.cells[oldId] == WALL ? COLORS.WALL : COLORS.EMPTY;
       drawCell(GRID.endCell.x, GRID.endCell.y);
@@ -235,7 +235,7 @@ function pageLoaded() {
     GRID.cells[id] = INPUT.brush == BRUSH.WALL ? WALL : EMPTY;
 
     drawCell(cords.x, cords.y);
-    INPUT.oldMouseCords = {x: cords.x, y: cords.y};
+    INPUT.oldMouseCords = { x: cords.x, y: cords.y };
   });
 
   canvas.addEventListener('mouseup', e => {
@@ -266,7 +266,7 @@ function frame() {
   window.requestAnimationFrame(frame);
 }
 
-const DijkstraSolver = function () {
+const DijkstraSolverOriginal = function () {
 
   this.neighbours = function (c) {
     var arr = [];
@@ -289,7 +289,7 @@ const DijkstraSolver = function () {
   }
 
   this.heuristics = function (c1, c2) {
-    if (SETTINGS.heuristics == 'NONE') return 0;
+    if (SETTINGS.heuristics == 'NONE') return 1;
 
     let c1x = c1 % GRID.width;
     let c1y = (c1 - c1x) / GRID.width;
@@ -411,6 +411,227 @@ const DijkstraSolver = function () {
         return;
       }
 
+    }
+
+  }
+
+}
+
+const DijkstraSolver = function () {
+
+  this.neighbours = function (c) {
+    var arr = [];
+
+    if (c >= GRID.width && this.visitedArr[c - GRID.width] != this.turn && GRID.cells[c - GRID.width] != WALL) {
+      arr.push(c - GRID.width);
+    }
+    if (c < GRID.width * (GRID.height - 1) && this.visitedArr[c + GRID.width] != this.turn && GRID.cells[c + GRID.width] != WALL) {
+      arr.push(c + GRID.width);
+    }
+    if (c % GRID.width != GRID.width - 1 && this.visitedArr[c + 1] != this.turn && GRID.cells[c + 1] != WALL) {
+      arr.push(c + 1);
+    }
+    if (c % GRID.width != 0 && this.visitedArr[c - 1] != this.turn && GRID.cells[c - 1] != WALL) {
+      arr.push(c - 1);
+    }
+
+    if (arr.length > 0) return arr;
+    else return null;
+  }
+
+  this.heuristics = function (c1, c2) {
+    if (SETTINGS.heuristics == 'NONE') return 1;
+
+    let c1x = c1 % GRID.width;
+    let c1y = (c1 - c1x) / GRID.width;
+    let c2x = c2 % GRID.width;
+    let c2y = (c2 - c2x) / GRID.width;
+
+    switch (SETTINGS.heuristics) {
+      case 'EUKLIDES': return Math.sqrt((c1x - c2x) * (c1x - c2x) + (c1y - c2y) * (c1y - c2y));
+      case 'MANHATTAN': return Math.abs(c1x - c2x) + Math.abs(c1y - c2y);
+      case 'CHEBYSHEW': return Math.max(Math.abs(c1x - c2x), Math.abs(c1y - c2y));
+      case 'OCTILE': return Math.max(Math.abs(c1x - c2x), Math.abs(c1y - c2y) + 1.414 * Math.min(Math.abs(c1x - c2x), Math.abs(c1y - c2y)));
+    }
+  }
+
+  this.cellWeight = function (c) {
+    if (SETTINGS.weighting == 'HOMOGENIC') return 1;
+    if (SETTINGS.weighting == 'RANDOM') return Math.random();
+
+    if (c >= GRID.width && GRID.cells[c - GRID.width] == WALL)
+      return 10;
+
+    if (c < GRID.width * (GRID.height - 1) && GRID.cells[c + GRID.width] == WALL)
+      return 10;
+
+    if (c % GRID.width != GRID.width - 1 && GRID.cells[c + 1] == WALL)
+      return 10;
+
+    if (c % GRID.width != 0 && GRID.cells[c - 1] == WALL)
+      return 10;
+
+
+    return 1;
+  }
+
+  this.pickCell = function () {
+    var cost = Infinity;
+    var index = undefined;
+
+
+    let goalCell = this.turn ? GRID.id(GRID.endCell.x, GRID.endCell.y) : GRID.id(GRID.startCell.x, GRID.startCell.y);
+
+    if (this.turn) {
+      for (let i = 0; i < this.queueStart.length; i++) {
+        if (this.costArr[this.queueStart[i]] + this.heuristics(this.queueStart[i], goalCell) < cost) {
+          cost = this.costArr[this.queueStart[i]] + this.heuristics(this.queueStart[i], goalCell);
+          index = i;
+        }
+      }
+      let cell = this.queueStart[index];
+      this.queueStart.splice(index, 1);
+      return cell;
+    }
+    else {
+      for (let i = 0; i < this.queueEnd.length; i++) {
+        if (this.costArr[this.queueEnd[i]] + this.heuristics(this.queueEnd[i], goalCell) < cost) {
+          cost = this.costArr[this.queueEnd[i]] + this.heuristics(this.queueEnd[i], goalCell);
+          index = i;
+        }
+      }
+      let cell = this.queueEnd[index];
+      this.queueEnd.splice(index, 1);
+      return cell;
+    }
+  }
+
+  this.init = function () {
+    this.finished = false;
+    this.costArr = new Array(GRID.width * GRID.height).fill(Infinity);
+    this.previousArr = new Array(GRID.width * GRID.height).fill(undefined);
+    this.visitedArr = new Array(GRID.width * GRID.height).fill(null);//null - unvisited, true - visitedStart, false - visited End
+
+    let startCell = GRID.id(GRID.startCell.x, GRID.startCell.y);
+    this.costArr[startCell] = 0;
+    this.queueStart = [startCell];
+
+    let endCell = GRID.id(GRID.endCell.x, GRID.endCell.y);
+    this.costArr[endCell] = 0;
+    this.queueEnd = [endCell]
+    ctx.fillStyle = COLORS.MARKED;
+
+    this.turn = true;//true - start, false - end
+  }
+
+  this.drawPath = function (connectionCell) {
+    ctx.fillStyle = COLORS.PATH;
+
+    var cell = connectionCell;
+    while (this.previousArr[cell] != undefined) {
+      let cx = cell % GRID.width;
+      let cy = (cell - cx) / GRID.width;
+      if (!(cx == GRID.startCell.x && cy == GRID.startCell.y))
+        drawCell(cx, cy);
+
+      cell = this.previousArr[cell];
+    }
+  }
+
+  this.update = function () {
+    for (let f = 0; f < SETTINGS.speed; f++) {
+    
+      if(this.turn){
+
+      if (this.queueStart.length > 0) {
+        var cell = this.pickCell();
+
+        if (this.visitedArr[cell] == this.turn || GRID.cells[cell] == WALL) {
+          f--;
+          continue;
+        }
+        /*if(this.visitedArr[cell] == !this.turn){
+          this.drawPath(cell);
+          this.finished = true;
+          document.getElementById('runButton').innerHTML = "CLEAR";
+          return;
+        }*/
+
+        let cx = cell % GRID.width;
+        let cy = (cell - cx) / GRID.width;
+        if (!(cx == GRID.startCell.x && cy == GRID.startCell.y))
+          drawCell(cx, cy);
+
+        this.visitedArr[cell] = this.turn;
+
+        var cell_n = this.neighbours(cell);
+        if (cell_n != null) {
+          for (let i = 0; i < cell_n.length; i++) {
+
+            var cell_weight = this.cellWeight(cell_n[i]);
+
+            if (this.costArr[cell] + cell_weight < this.costArr[cell_n[i]]) {
+              this.costArr[cell_n[i]] = this.costArr[cell] + cell_weight;
+              this.previousArr[cell_n[i]] = cell;
+            }
+            this.queueStart.push(cell_n[i]);
+          }
+        }
+      }
+      else {
+        console.log(4);
+        this.finished = true;
+        document.getElementById('runButton').innerHTML = "CLEAR";
+        continue;
+      }
+
+      }else{
+        if (this.queueEnd.length > 0) {
+          var cell = this.pickCell();
+  
+          if (this.visitedArr[cell] == this.turn || GRID.cells[cell] == WALL) {
+            f--;
+            continue;
+          }
+
+          /*if(this.visitedArr[cell] == !this.turn){
+            this.drawPath(cell);
+            this.finished = true;
+            document.getElementById('runButton').innerHTML = "CLEAR";
+            return;
+          }*/
+  
+          let cx = cell % GRID.width;
+          let cy = (cell - cx) / GRID.width;
+          if (!(cx == GRID.startCell.x && cy == GRID.startCell.y))
+            drawCell(cx, cy);
+  
+          this.visitedArr[cell] = this.turn;
+  
+          var cell_n = this.neighbours(cell);
+          if (cell_n != null) {
+            for (let i = 0; i < cell_n.length; i++) {
+  
+              var cell_weight = this.cellWeight(cell_n[i]);
+  
+              if (this.costArr[cell] + cell_weight < this.costArr[cell_n[i]]) {
+                this.costArr[cell_n[i]] = this.costArr[cell] + cell_weight;
+                this.previousArr[cell_n[i]] = cell;
+                console.log(this.turn, this.visitedArr[cell_n[i]]);
+              }
+              this.queueEnd.push(cell_n[i]);
+            }
+          }
+        }
+        else {
+          console.log(4);
+          this.finished = true;
+          document.getElementById('runButton').innerHTML = "CLEAR";
+          continue;
+        }
+      }
+
+      this.turn = !this.turn;
     }
 
   }
