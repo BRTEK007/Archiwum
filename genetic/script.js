ad47b6f58256cb68d2bca4a80eb5615159948bb4
BRTEK007 <bartoszkonderla@gmail.com>
2021-06-12 14:35:34 +0200

    blocks rendering, Course class

--- genetic/script.js
+++ genetic/script.js
@@ -3,7 +3,7 @@
 var canvas, ctx;
 var painter;
 
-var blocks = [];
+var course;
 var population;
 
 const GRID = {
@@ -12,9 +12,93 @@ const GRID = {
   height: 25,
   offset_x: 10,
   offset_y: 10,
-  startCell: { x: 0, y: 0 },
-  endCell: { x: 2, y: 0 },
   id: (x, y) => x + y * GRID.width
+}
+
+function renderHorizontalBlock(_gx, _gy){
+  let x = GRID.offset_x + _gx * (GRID.cell_size + 1) + 1;
+  let y = GRID.offset_y + _gy * (GRID.cell_size + 1)+ 1;
+  let w = GRID.cell_size -1;
+  let h = GRID.cell_size -1;
+
+  ctx.fillStyle = "white";
+  ctx.beginPath();
+  ctx.rect(x, y, w, h);
+  ctx.fill();
+
+  ctx.lineWidth = h*0.05;
+  ctx.strokeStyle = 'yellow';
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + w, y);
+  ctx.moveTo(x, y + h);
+  ctx.lineTo(x + w, y + h);
+  ctx.stroke();
+}
+
+function renderLeftSideRoundedBlock(_gx, _gy){
+  let x = GRID.offset_x + (_gx+0.5) * (GRID.cell_size + 1) + 1;
+  let y = GRID.offset_y + _gy* (GRID.cell_size + 1)+ 1;
+  let w = GRID.cell_size/2 -1;
+  let h = GRID.cell_size -1;
+
+
+  ctx.fillStyle = "green";
+  ctx.rect(x, y, w, h);
+  ctx.fill();
+
+  ctx.lineWidth = h*0.05;
+  ctx.strokeStyle = 'yellow';
+  ctx.beginPath();
+  ctx.arc(x+1, y + GRID.cell_size/2, GRID.cell_size/2, 0.5* Math.PI,  1.5 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + w, y);
+  ctx.moveTo(x, y + h);
+  ctx.lineTo(x + w, y + h);
+  ctx.stroke();
+
+}
+
+function renderRightSideRoundedBlock(_gx, _gy){
+
+}
+
+class Course{
+  constructor(){
+    this.blocks = [];
+
+    for(let x = 0; x < GRID.width; x++){
+      var b = new Block(x, Math.floor(GRID.height/2));
+      this.blocks.push(b);
+    }
+
+    this.blocks[0].index = 0;
+    this.blocks[0].color = "green";
+    this.blocks[0].next = this.blocks[1];
+    this.blocks[0].setWalls();
+
+    for(let i = 1; i < this.blocks.length-1; i++){
+      this.blocks[i].prev = this.blocks[i-1];
+      this.blocks[i].next = this.blocks[i+1];
+      this.blocks.index = i;
+      this.blocks[i].setWalls();
+    }
+
+    this.blocks[this.blocks.length-1].index = this.blocks.length-1;
+    this.blocks[this.blocks.length-1].color = "red";
+    this.blocks[this.blocks.length-1].prev = this.blocks[this.blocks.length-2];
+    this.blocks[this.blocks.length-1].setWalls();
+
+  }
+  render(){
+    for(let i = 0; i < this.blocks.length; i++){
+      this.blocks[i].render();
+    }
+  }
 }
 
 class Block{
@@ -23,19 +107,28 @@ class Block{
     this.next = null;
     this.prev = null;
     this.index = null;
+    this.color = "white";
+    this.walls = [];
   }
   render(){
-    let x = GRID.offset_x + this.gridPos.x * (GRID.cell_size + 1) + 1;
-    let y = GRID.offset_y + this.gridPos.y * (GRID.cell_size + 1)+ 1;
+    let x = GRID.offset_x + this.gridPos.x * (GRID.cell_size + 1) +1;
+    let y = GRID.offset_y + this.gridPos.y * (GRID.cell_size + 1) +1; 
     let w = GRID.cell_size -1;
     let h = GRID.cell_size -1;
-    if(this.prev == null)
-      ctx.fillStyle = "green";
-    else if(this.next == null)
-      ctx.fillStyle = "red";
-    else
-      ctx.fillStyle = "white";
-    ctx.fillRect(x, y, w, h);
+
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.rect(x, y, w, h);
+    ctx.fill();
+
+    ctx.lineWidth = h*0.05;
+    ctx.strokeStyle = 'yellow';
+    ctx.beginPath();
+    for(let i = 0; i < this.walls.length; i++){
+      ctx.moveTo(GRID.offset_x + this.walls[i].x1 * (GRID.cell_size + 1) + 1, GRID.offset_y + this.walls[i].y1 * (GRID.cell_size + 1)+ 1);
+      ctx.lineTo(GRID.offset_x + this.walls[i].x2 * (GRID.cell_size + 1) + 1, GRID.offset_y + this.walls[i].y2 * (GRID.cell_size + 1)+ 1);
+    }
+    ctx.stroke();
   }
   isOnBlock(_ax, _ay){
     let lx = this.gridPos.x * (GRID.cell_size+1) + GRID.offset_x;
@@ -52,28 +145,64 @@ class Block{
   }
   getCoverRate(_ax, _ay){
     if(this.next == null) return 0;
-    //check for next block orientaion
+    //check for next block orientaion, AKA distance to the next blocks wall
     let lx = this.gridPos.x * (GRID.cell_size+1) + GRID.offset_x;
     let rx = (this.gridPos.x+1) * (GRID.cell_size+1) + GRID.offset_x;
     return (_ax - lx) / (rx-lx);
   }
-  setIndexNext(_i){
-    this.index = _i;
-    if(this.next != null)
-      this.next.setIndexNext(_i+1);
+  setWalls(){
+    var isWall = [true, true, true, true];
+    if(this.next != null){
+
+      if(this.next.gridPos.y == this.gridPos.y){
+        if(this.next.gridPos.x > this.gridPos.x)
+          isWall[2] = false;
+        else
+          isWall[0] = false;
+      }else{
+        if(this.next.gridPos.y > this.gridPos.y)
+          isWall[3] = false;
+        else
+          isWall[1] = false;
+      }
+
+    }
+
+    if(this.prev != null){
+
+      if(this.prev.gridPos.y == this.gridPos.y){
+        if(this.prev.gridPos.x > this.gridPos.x)
+          isWall[2] = false;
+        else
+          isWall[0] = false;
+      }else{
+        if(this.prev.gridPos.y > this.gridPos.y)
+          isWall[3] = false;
+        else
+          isWall[1] = false;
+      }
+
+    }
+
+    if(isWall[0] == true) this.walls.push({x1: this.gridPos.x, y1: this.gridPos.y, x2: this.gridPos.x, y2: this.gridPos.y+1});
+    if(isWall[1] == true) this.walls.push({x1: this.gridPos.x, y1: this.gridPos.y, x2: this.gridPos.x+1, y2: this.gridPos.y}); 
+    if(isWall[2] == true) this.walls.push({x1: this.gridPos.x+1, y1: this.gridPos.y, x2: this.gridPos.x+1, y2: this.gridPos.y+1}); 
+    if(isWall[3] == true) this.walls.push({x1: this.gridPos.x, y1: this.gridPos.y+1, x2: this.gridPos.x+1, y2: this.gridPos.y+1});  
   }
 }
 
 class Population{
-  constructor(_startBlock){
-    this.startBlock = _startBlock;
-    this.startPos = {x: (_startBlock.gridPos.x+0.5) * (GRID.cell_size+1) + GRID.offset_x, y: (_startBlock.gridPos.y+0.5) * (GRID.cell_size+1) +  + GRID.offset_y};
+  constructor(_course){
+    this.startBlock = _course.blocks[0];
+    this.startPos = {x: (this.startBlock.gridPos.x+0.5) * (GRID.cell_size+1) + GRID.offset_x, y: (this.startBlock.gridPos.y+0.5) * (GRID.cell_size+1) +  + GRID.offset_y};
     this.populationSize = 20;
     this.iteration = 0;
     this.mutationRate = 0.05;
+    this.cutOffRate = 0.1;
+    this.genesSize = _course.blocks.length*20;
     this.agents = new Array(this.populationSize);
     for(let i = 0; i < this.populationSize; i++){
-      this.agents[i] = new Agent(this.startBlock, this.startPos.x, this.startPos.y);
+      this.agents[i] = new Agent(this.startBlock, this.startPos.x, this.startPos.y, this.genesSize);
       this.agents[i].generateRandomGenes();
     }
   }
@@ -104,8 +233,9 @@ class Population{
     });
     //var indexPoll = new Array(this.populationSize*(this.populationSize+1)/2);
     var indexPoll = [];
-    for(let i = 0; i < this.populationSize; i++){
-      for(let j = i*i; j < this.populationSize*this.populationSize; j++){
+    var afterCutOffSize = Math.floor((1-this.cutOffRate) * this.populationSize);
+    for(let i = 0; i <  afterCutOffSize; i++){
+      for(let j = i*i; j < afterCutOffSize*afterCutOffSize; j++){
         indexPoll.push(i);
       }
     }
@@ -125,7 +255,7 @@ class Population{
 
   resetPopulation(){
     for(let i = 0; i < this.populationSize; i++){
-      this.agents[i] = new Agent(this.startBlock, this.startPos.x, this.startPos.y);
+      this.agents[i] = new Agent(this.startBlock, this.startPos.x, this.startPos.y, this.genesSize);
       this.agents[i].generateRandomGenes();
     }
   }
@@ -140,7 +270,7 @@ class Population{
   }
 
   getChildAgent(_a1, _a2){
-    var child = new Agent(this.startBlock, this.startPos.x, this.startPos.y);
+    var child = new Agent(this.startBlock, this.startPos.x, this.startPos.y, this.genesSize);
     for(let i = 0; i < child.genes.length; i++){
       let r = Math.random();
       if(r < this.mutationRate)
@@ -156,17 +286,17 @@ class Population{
 }
 
 class Agent{
-  constructor(_startBlock, _x, _y){
+  constructor(_startBlock, _x, _y, _gensSize){
     this.vel = {x : 0, y : 0};
     this.pos = {x: _x, y : _y};
     this.currentBlock = _startBlock;
     this.furthestBlockReachedIndex = _startBlock.index;
     this.furthestBlockCoverRate = 0;
     this.finished = false;
-    this.genes = new Array(100);
+    this.genes = new Array(_gensSize);
   }
   update(_iteration){
-    if(_iteration >= 100){ 
+    if(_iteration >= this.genes.length){ 
       this.finished = true; 
       return
     }
@@ -195,7 +325,7 @@ class Agent{
     //console.log(this.genes);
   }
   generateRandomGenes(){
-    for(let i = 0; i < 100; i++){
+    for(let i = 0; i < this.genes.length; i++){
       this.genes[i] = {
         x : Math.random()*2 -1,
         y : Math.random()*2 -1
@@ -227,25 +357,10 @@ function pageLoaded() {
   ctx = canvas.getContext('2d');
 
   updateGridDimensions();
-  drawGrid();
+  //drawGrid();
 
-  for(let x = GRID.startCell.x; x <= GRID.endCell.x; x++){
-    var b = new Block(x, GRID.startCell.y);
-    if(x > GRID.startCell.x){
-      b.prev = blocks[blocks.length -1];
-      blocks[blocks.length-1].next = b;
-    }
-    blocks.push(b);
-  }
-  blocks[0].setIndexNext(0);
-  population = new Population(blocks[0]);
-
-  //drawCell(GRID.startCell.x, GRID.startCell.y, "green");
-  //drawCell(GRID.endCell.x, GRID.endCell.y, "red");
-
-  /*for(let x = GRID.startCell.x+1; x < GRID.endCell.x; x++){
-    drawCell(x, GRID.endCell.y, "white");
-  }*/
+  course = new Course();
+  population = new Population(course);
 
   window.requestAnimationFrame(frame);
 }
@@ -255,13 +370,11 @@ function updateGridDimensions() {
   GRID.height = Math.floor((canvas.height - GRID.cell_size*0.5) / (GRID.cell_size + 1));
   GRID.offset_x = Math.floor((canvas.width - (GRID.cell_size + 1) * GRID.width) / 2);
   GRID.offset_y = Math.floor((canvas.height - (GRID.cell_size + 1) * GRID.height) / 2);
-  GRID.startCell = { x: Math.round(GRID.width * 0.3333), y: Math.round(GRID.height / 2) };
-  GRID.endCell = { x: Math.round(GRID.width * 0.6666), y: Math.round(GRID.height / 2) };
   GRID.cells = new Array(GRID.width * GRID.height).fill(0);
 }
 
 function drawGrid() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.lineWidth = 1;
   ctx.strokeStyle = 'white';
   for (var x = 0; x <= GRID.width; x++) {
     ctx.beginPath();
@@ -280,10 +393,10 @@ function drawGrid() {
 function frame() {
   window.requestAnimationFrame(frame);
   ctx.clearRect(0,0, canvas.width, canvas.height);
-  for(let i = 0; i < blocks.length; i++){
-    blocks[i].render();
-  }
 
- population.update();
- population.render(); 
+  course.render();
+  population.update();
+  population.render();
+  
+  //drawGrid();
 }
