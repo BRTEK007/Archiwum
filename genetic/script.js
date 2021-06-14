6abde6e5c9e86b0ed6058baf537f7887b6d0630d
BRTEK007 <bartoszkonderla@gmail.com>
2021-06-14 15:27:44 +0200

    fitness

--- genetic/script.js
+++ genetic/script.js
@@ -71,10 +71,33 @@ class Course{
   constructor(){
     this.blocks = [];
 
-    for(let x = 0; x < GRID.width; x++){
-      var b = new Block(x, Math.floor(GRID.height/2));
-      this.blocks.push(b);
-    }
+
+    let b = new Block(0,0);
+    this.blocks.push(b);
+    b = new Block(1, 0);
+    this.blocks.push(b);
+    b = new Block(2, 0);
+    this.blocks.push(b);
+    b = new Block(3, 0);
+    this.blocks.push(b);
+    b = new Block(3, 1);
+    this.blocks.push(b);
+    b = new Block(3, 2);
+    this.blocks.push(b);
+    b = new Block(3, 3);
+    this.blocks.push(b);
+    /*b = new Block(2, 1);
+    this.blocks.push(b);
+    b = new Block(1, 1);
+    this.blocks.push(b);
+    b = new Block(0, 1);
+    this.blocks.push(b);
+    b = new Block(0, 2);
+    this.blocks.push(b);
+    b = new Block(1, 2);
+    this.blocks.push(b);
+    b = new Block(2, 2);
+    this.blocks.push(b);*/
 
     this.blocks[0].index = 0;
     this.blocks[0].color = "green";
@@ -84,7 +107,7 @@ class Course{
     for(let i = 1; i < this.blocks.length-1; i++){
       this.blocks[i].prev = this.blocks[i-1];
       this.blocks[i].next = this.blocks[i+1];
-      this.blocks.index = i;
+      this.blocks[i].index = i;
       this.blocks[i].setWalls();
     }
 
@@ -105,6 +128,7 @@ class Block{
   constructor(_x, _y){
     this.gridPos = {x : _x, y : _y};
     this.next = null;
+    this.nextBlockOrientation = null;
     this.prev = null;
     this.index = null;
     this.color = "white";
@@ -129,6 +153,13 @@ class Block{
       ctx.lineTo(GRID.offset_x + this.walls[i].x2 * (GRID.cell_size + 1) + 1, GRID.offset_y + this.walls[i].y2 * (GRID.cell_size + 1)+ 1);
     }
     ctx.stroke();
+
+    if(this.next == null){
+      ctx.fillStyle = 'yellow';
+      ctx.beginPath();
+      ctx.arc(x + w/2, y + h/2, GRID.cell_size*0.2, 0, 2 * Math.PI);
+      ctx.fill();
+    }
   }
   isOnBlock(_ax, _ay){
     let lx = this.gridPos.x * (GRID.cell_size+1) + GRID.offset_x;
@@ -145,27 +176,42 @@ class Block{
   }
   getCoverRate(_ax, _ay){
     if(this.next == null) return 0;
-    //check for next block orientaion, AKA distance to the next blocks wall
+
     let lx = this.gridPos.x * (GRID.cell_size+1) + GRID.offset_x;
     let rx = (this.gridPos.x+1) * (GRID.cell_size+1) + GRID.offset_x;
-    return (_ax - lx) / (rx-lx);
+    let ty = this.gridPos.y * (GRID.cell_size+1) + GRID.offset_y;
+    let by = (this.gridPos.y+1) * (GRID.cell_size+1) + GRID.offset_y;
+    
+    switch(this.nextBlockOrientation){
+      case 0: return (rx - _ax) / (rx-lx);
+      case 1: return (by - _ay) / (by-ty);
+      case 2: return (_ax - lx) / (rx-lx);
+      case 3: return (_ay - ty) / (by-ty); 
+    }
   }
   setWalls(){
     var isWall = [true, true, true, true];
     if(this.next != null){
 
       if(this.next.gridPos.y == this.gridPos.y){
-        if(this.next.gridPos.x > this.gridPos.x)
+        if(this.next.gridPos.x > this.gridPos.x){
           isWall[2] = false;
-        else
+          this.nextBlockOrientation = 2;
+        }
+        else{
           isWall[0] = false;
+          this.nextBlockOrientation = 0;
+        }
       }else{
-        if(this.next.gridPos.y > this.gridPos.y)
+        if(this.next.gridPos.y > this.gridPos.y){
           isWall[3] = false;
-        else
+          this.nextBlockOrientation = 3;
+        }
+        else{
           isWall[1] = false;
+          this.nextBlockOrientation = 1;
+        }
       }
-
     }
 
     if(this.prev != null){
@@ -189,17 +235,25 @@ class Block{
     if(isWall[2] == true) this.walls.push({x1: this.gridPos.x+1, y1: this.gridPos.y, x2: this.gridPos.x+1, y2: this.gridPos.y+1}); 
     if(isWall[3] == true) this.walls.push({x1: this.gridPos.x, y1: this.gridPos.y+1, x2: this.gridPos.x+1, y2: this.gridPos.y+1});  
   }
+  checkForGoal(_ax, _ay){
+    if(this.next != null) return false;
+
+    let cx = GRID.offset_x + (this.gridPos.x+0.5) * (GRID.cell_size + 1) +1;
+    let cy = GRID.offset_y + (this.gridPos.y+0.5) * (GRID.cell_size + 1) +1;
+    
+    return (Math.sqrt(Math.pow(cx - _ax, 2) + Math.pow(cy - _ay, 2)) <= GRID.cell_size*0.2);
+  }
 }
 
 class Population{
   constructor(_course){
     this.startBlock = _course.blocks[0];
     this.startPos = {x: (this.startBlock.gridPos.x+0.5) * (GRID.cell_size+1) + GRID.offset_x, y: (this.startBlock.gridPos.y+0.5) * (GRID.cell_size+1) +  + GRID.offset_y};
-    this.populationSize = 20;
+    this.populationSize = 50;
     this.iteration = 0;
     this.mutationRate = 0.05;
-    this.cutOffRate = 0.1;
-    this.genesSize = _course.blocks.length*20;
+    this.cutOffRate = 0.0;
+    this.genesSize = _course.blocks.length*30;
     this.agents = new Array(this.populationSize);
     for(let i = 0; i < this.populationSize; i++){
       this.agents[i] = new Agent(this.startBlock, this.startPos.x, this.startPos.y, this.genesSize);
@@ -215,21 +269,52 @@ class Population{
         this.finishedAgents++; 
     }
     this.iteration++;
-    if(this.finishedAgents == this.populationSize){
+    if(this.iteration == this.genesSize || this.finishedAgents == this.populationSize){
       this.evolvePopulation();
       this.iteration = 0;
     }
   }
 
   evolvePopulation(){
-    var sortedAgents = [...this.agents];
-    sortedAgents.sort(function(a,b){
-      if(a.furthestBlockReachedIndex > b.furthestBlockReachedIndex) return -1;
-      else if(a.furthestBlockReachedIndex == b.furthestBlockReachedIndex){
-        if(a.furthestBlockCoverRate > b.furthestBlockCoverRate) return -1;
-        else return 1;
+    var fitnessSum = 0.0;
+    var bestAgent = this.agents[0];
+    for(let i = 0; i < this.agents.length; i++){
+      if(this.agents[i].hasReachedGoal)
+        this.agents[i].fitness = this.genesSize/30 + 1 - this.agents[i].stepsToReachGoal/this.genesSize;
+      else
+        this.agents[i].fitness = (this.agents[i].furthestBlockReachedIndex + this.agents[i].furthestBlockCoverRate) * 0.8;
+
+      fitnessSum += this.agents[i].fitness;
+      if(this.agents[i].fitness > bestAgent.fitness) bestAgent = this.agents[i];
+    }
+    var newAgents = new Array(this.populationSize);
+    newAgents[0] = new Agent(this.startBlock, this.startPos.x, this.startPos.y);
+    newAgents[0].genes = [...bestAgent.genes];
+
+    for(let i = 1; i < this.populationSize; i++){
+      let rSum = Math.random() * fitnessSum;
+      let a = this.getAgentByFitnessSum(rSum);  
+      newAgents[i] = new Agent(this.startBlock, this.startPos.x, this.startPos.y);//overrites fitness
+      newAgents[i].genes = [...a.genes];
+      newAgents[i].mutateGenes(this.mutationRate); 
+    }
+    this.agents = newAgents;
+  }
+
+    getAgentByFitnessSum(_rs) {
+      var fSum = 0.0;
+      for(let i = 0; i < this.populationSize; i++){
+        fSum += this.agents[i].fitness;
+        if(fSum > _rs)
+          return this.agents[i];
       }
-      else return 1;
+      console.log('lul');
+    }
+
+
+    /*var sortedAgents = [...this.agents];
+    sortedAgents.sort(function(a,b){
+      return b.fitness - a.fitness;
     });
     //var indexPoll = new Array(this.populationSize*(this.populationSize+1)/2);
     var indexPoll = [];
@@ -240,18 +325,21 @@ class Population{
       }
     }
     this.agents[0] = new Agent(this.startBlock, this.startPos.x, this.startPos.y);
-    this.agents[0].genes = sortedAgents[0].genes;
+    this.agents[0].genes = [...sortedAgents[0].genes];
     for(let i = 1; i < this.populationSize; i++){
       var p1 = sortedAgents[ indexPoll[ Math.floor(Math.random() * indexPoll.length)] ];
       var p2 = sortedAgents[ indexPoll[ Math.floor(Math.random() * indexPoll.length)] ];
-      while(p2 == p1){
+      /*while(p2 == p1){
         p2 = sortedAgents[ indexPoll[ Math.floor(Math.random() * indexPoll.length)] ];
       }
       var a = this.getChildAgent(p1, p2);
       this.agents[i] = a;
-    }
+      this.agents[i] = new Agent(this.startBlock, this.startPos.x, this.startPos.y, this.genesSize);
+      this.agents[i].genes = [...this.agents[0].genes];
+      this.agents[i].mutateGenes(0.2);
+    }*/
 
-  }
+  
 
   resetPopulation(){
     for(let i = 0; i < this.populationSize; i++){
@@ -262,11 +350,15 @@ class Population{
 
   render(){
     ctx.fillStyle = "blue";
-    for(let i = 0; i < this.agents.length; i++){
+    for(let i = 1; i < this.agents.length; i++){
       ctx.beginPath();
       ctx.arc(this.agents[i].pos.x, this.agents[i].pos.y, 10, 0, 2 * Math.PI);
       ctx.fill();
     }
+    ctx.fillStyle = "magenta";
+    ctx.beginPath();
+    ctx.arc(this.agents[0].pos.x, this.agents[0].pos.y, 15, 0, 2 * Math.PI);
+    ctx.fill();
   }
 
   getChildAgent(_a1, _a2){
@@ -292,18 +384,25 @@ class Agent{
     this.currentBlock = _startBlock;
     this.furthestBlockReachedIndex = _startBlock.index;
     this.furthestBlockCoverRate = 0;
+    this.hasReachedGoal = false;
+    this.stepsToReachGoal = 0;
     this.finished = false;
+    this.fitness = 0.0;
     this.genes = new Array(_gensSize);
   }
   update(_iteration){
-    if(_iteration >= this.genes.length){ 
-      this.finished = true; 
-      return
+    //check for reached goal
+    if(this.currentBlock.checkForGoal(this.pos.x, this.pos.y)){
+      this.hasReachedGoal = true;
+      this.finished = true;
+      this.stepsToReachGoal = _iteration;
+      return;
     }
     //calculate cover rate
     if(this.currentBlock.isOnBlock(this.pos.x, this.pos.y)){
       this.furthestBlockCoverRate = Math.max(this.furthestBlockCoverRate, this.currentBlock.getCoverRate(this.pos.x, this.pos.y));
-    }  
+      this.furthestBlockCoverRate = this.currentBlock.getCoverRate(this.pos.x, this.pos.y);
+    }
     else{
       if(this.currentBlock.next != null && this.currentBlock.next.isOnBlock(this.pos.x, this.pos.y)){
         this.currentBlock = this.currentBlock.next;
@@ -322,16 +421,36 @@ class Agent{
     this.vel.y += this.genes[_iteration].y;
     this.pos.x += this.vel.x;
     this.pos.y += this.vel.y;
+    //this.pos.x += this.genes[_iteration].x;
+    //this.pos.y += this.genes[_iteration].y;
     //console.log(this.genes);
   }
   generateRandomGenes(){
     for(let i = 0; i < this.genes.length; i++){
+      let a = Math.random() * Math.PI * 2;
       this.genes[i] = {
-        x : Math.random()*2 -1,
-        y : Math.random()*2 -1
+        x : Math.cos(a),
+        y : Math.sin(a)
       };
     }
   }
+  mutateGenes(_mr){
+    for(let i = 0; i < this.genes.length; i++){
+      let r = Math.random();
+      if(r < _mr){
+        let a = Math.random() * Math.PI * 2;
+        this.genes[i] = {
+          x : Math.cos(a),
+          y : Math.sin(a)
+        };
+      }
+    }
+  }
+
+  calculateFitness(){
+
+  }
+
 }
 
 function pageLoaded() {
@@ -395,6 +514,7 @@ function frame() {
   ctx.clearRect(0,0, canvas.width, canvas.height);
 
   course.render();
+  //for(let i = 0; i < 10; i++)
   population.update();
   population.render();
   
